530e4a90f95b5f7de379116589450f6a892e5fdb
alwiyahya99 <alwizero404@gmail.com>
2023-04-05 14:49:56 +0700

    Initial Commit

--- mydb.py
+++ mydb.py
@@ -4,6 +4,18 @@
 # pip install mysql-connector
 # pip install mysql-connector-python
 
+# git config --global user.name "a.........9"
+# git config --global user.email "a.........@gmail.com"
+# git config --global push.default matching
+# git config --global alias.co checkout
+# git init
+# git add .
+# git commit -am 'Initial Commint'
+# git remote add origin https://github.com/alwiyahya99/Django-CRM.git
+# git branch -M main
+# git push -u origin main
+
+
 import mysql.connector
 
 dataBase = mysql.connector.connect(
