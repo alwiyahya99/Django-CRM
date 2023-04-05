3e9c32a196c32ec49a87a2e2fba14cfbaf8e1224
alwiyahya99 <alwizero404@gmail.com>
2023-04-05 07:02:23 +0700

    Initial Commint

--- mydb.py
+++ mydb.py
@@ -1,3 +1,9 @@
+# Install Mysql on your computer
+# https://dev.mysql.com/downloads/installer/
+# pip install mysql
+# pip install mysql-connector
+# pip install mysql-connector-python
+
 import mysql.connector
 
 dataBase = mysql.connector.connect(
