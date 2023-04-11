e9b04fcef00d1c037b81f284511f1799cbbbdff6
alwiyahya99 <alwizero404@gmail.com>
2023-04-12 05:13:54 +0700

    Initial Commit

--- website/urls.py
+++ website/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     # path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
+    path('keuangan/', views.keuangan, name='keuangan'),
+    path('pekerjaan/', views.pekerjaan, name='pekerjaan'),
+    path('pegawai/', views.pegawai, name='pegawai'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.template import loader
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
@@ -28,3 +30,16 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out successfully")
     return redirect('home')
+
+def keuangan(request):
+    template = loader.get_template('keuangan.html')
+    context = {
+        'name_page': 'Keuangan',
+    }
+    return HttpResponse(template.render(context, request))   
+
+def pekerjaan(request):
+    return render(request, 'pekerjaan.html')
+
+def pegawai(request):
+    return render(request, 'pegawai.html')
