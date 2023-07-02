36efd61c1f6376f9662c569a5f705d3a547c7132
alwiyahya99 <alwizero404@gmail.com>
2023-07-02 07:31:30 +0700

    model phone

--- website/models.py
+++ website/models.py
@@ -6,6 +6,7 @@ class Record(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=15, null=True)
     address = models.CharField(max_length=100)
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=20)
